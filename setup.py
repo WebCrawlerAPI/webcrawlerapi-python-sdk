dc2dc3d5d4d7a5ce1c83ada4ac044921918b65c4
AndeyM <andrey@mysterium.network>
2025-06-12 07:31:11 +0200

    Update version to v2.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="webcrawlerapi",
-    version="2.0.4",
+    version="2.0.5",
     packages=find_packages(),
     install_requires=[
         "requests>=2.25.0",
